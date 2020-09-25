1fad19c3a19a7572fcc8e4b2de7154b793a07cb0
Gaurav Tewari <gauravtewari111@gmail.com>
2020-09-26 00:11:00 +0530

    finally done

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,71 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            verticalDirection: VerticalDirection.down,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                margin: EdgeInsets.only(left: 30.0),
-                padding: EdgeInsets.all(20),
-                child: Text('container1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                    'https://avatars3.githubusercontent.com/u/54894783?s=460&u=8a0c5e75425e17ca3e2e3dad7b8c9c17d66bcc83&v=4'),
               ),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue,
-                  child: Text('Container 2')),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Text('Container 3')),
+              Text(
+                'Gaurav Tewari',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Student',
+                style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontSize: 25.0,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+9898989898',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans',
+                        fontSize: 25.0),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'tewarig0@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans',
+                        fontSize: 25.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
